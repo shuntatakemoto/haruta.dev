7008c70ef26579d3b9edb2e796b9280b93b6abe5
shuntatakemoto <rt80405@gmail.com>
2022-02-14 18:57:11 +0900

    blogのリンクを追加

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,7 @@
 const routesNav = [
   // { name: 'Top', path: '/', route: true },
   { name: 'About', path: '/about', route: true },
+  { name: ' Blog', path: 'https://haruta-blog.vercel.app/', route: false },
   { name: 'Github', path: 'https://github.com/shuntatakemoto', route: false },
   { name: 'Twitter', path: 'https://twitter.com/haruta_8_', route: false },
   { name: 'Zenn', path: 'https://zenn.dev/shuntatakemoto', route: false },
