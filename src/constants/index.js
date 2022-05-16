1329115620a9f78b62642b83cd718a2294e62349
shuntatakemoto <rt80405@gmail.com>
2022-05-16 23:26:43 +0900

    🔥 Zennのリンクを削除

--- src/constants/index.js
+++ src/constants/index.js
@@ -4,7 +4,6 @@ const routesNav = [
   { name: ' Blog', path: 'https://haruta-blog.vercel.app/', route: false },
   { name: 'Github', path: 'https://github.com/shuntatakemoto', route: false },
   { name: 'Twitter', path: 'https://twitter.com/haruta_8_', route: false },
-  { name: 'Zenn', path: 'https://zenn.dev/shuntatakemoto', route: false },
 ];
 
 export { routesNav };
